c994b628fce7faeb64a519a9c16af0dbf09c8a7e
NovaBiriseg <sinistros.novabiriseg@gmail.com>
2025-07-15 10:53:52 -0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -377,3 +377,6 @@ logger = logging.getLogger(__name__)
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+    @app.get("/")
+async def root():
+    return {"message": "Backend online"}
